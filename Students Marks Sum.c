ff03547002fa5d061416664e466c0665f23dca20
SoumikCoding <soumik1410code@gmail.com>
2024-07-24 12:04:32 +0530

    Create Students Marks Sum.c

---
+++ Students Marks Sum.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int start=0;
+  if(gender=='g')
+    start++;
+  int sum=0;
+  while(start<number_of_students)
+  {
+      sum+=marks[start];
+      start+=2;
+  }
+  return sum;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
